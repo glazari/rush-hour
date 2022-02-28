30dba646926ccb93e304ababdcff18b612d0b56c
Guilherme Lazari <guilherme.lazari@wildlifestudios.com>
2022-02-28 13:14:56 -0300

    Draw arbitrary number of cars

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,51 @@
 fn main() {
-    print!("\x1b[2J"); // clear screen
-    print!("\x1b[0;0H"); // move cursor to top
+    let game = Game {
+        cars: vec![
+            Car::new(1, 196, false, (0, 0)),
+            Car::new(22, 64, true, (2, 2)),
+        ],
+    };
+    game.draw()
+}
 
-    println!(" _________ ");
-    println!("| o o o o |");
-    println!("| o o o o  ");
-    println!("| o o o o |");
-    println!("| o o o o |");
-    println!(" _________ ");
+struct Game {
+    cars: Vec<Car>,
+}
 
-    // draw horizontal car
-    let car = Car::new(1, 196, false, (0, 0));
-    car.draw();
+impl Game {
+    fn draw(&self) {
+        print!("\x1b[2J"); // clear screen
+        print!("\x1b[0;0H"); // move cursor to top
 
-    let car = Car::new(22, 64, true, (2, 1));
-    car.draw();
+        println!(" _________ ");
+        println!("| o o o o |");
+        println!("| o o o o  ");
+        println!("| o o o o |");
+        println!("| o o o o |");
+        println!(" _________ ");
 
-    print!("\x1b[10;10H\n"); // move cursor to bottom
+        for car in self.cars.iter() {
+            Game::draw_car(car);
+        }
+        print!("\x1b[10;10H\n"); // move cursor to bottom
+    }
+
+    fn draw_car(car: &Car) {
+        let (x, y) = (car.position.0 + 2, (car.position.1 * 2) + 3);
+        print!("\x1b[{};{}H", x, y); // move cursor
+
+        if car.vertical {
+            println!(
+                "\x1b[48;5;{}m\x1b[38;5;{}m^\x1b[1B\x1b[1D|\x1b[1B\x1b[0m",
+                car.color, car.background
+            );
+        } else {
+            print!(
+                "\x1b[48;5;{}m\x1b[38;5;{}m-->\x1b[0m",
+                car.color, car.background
+            );
+        }
+    }
 }
 
 struct Car {
